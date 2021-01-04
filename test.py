a89e8a79f82f9b9a8b806cbfb2d241cace1a1a4a
lixs03 <lixs03@mingyuanyun.com>
2021-01-04 19:53:36 +0800

    测试更新代码

--- test.py
+++ test.py
@@ -10,50 +10,10 @@ from xml.sax import saxutils
 # Case_xls = Get_excel.read_casefile_2("Billingrules.xlsx", "Billingrules")
 # case=[['querynumber', 'post', '0', '{"current":1,"bh":"20200807151805","size":10}', '0', '0000', '20200807151805'], ['querytype', 'post', '0', '{"current":1,"gzmc":"无照经营","qtbm":"","size":10}', '0', '0000', '无照经营游商']]
 
-# 冒泡排序
-def bubble_sort(arr):
-    length = len(arr)
-    for i in range(0,length-1):
-        for j in range(0,length-i-1):
-            if arr[j]>arr[j+1]:
-                t = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = t
-def get_random_list(n):
-    l = []
-    for i in range(0,n):
-        l.append(random.randint(1,10))
-    return l
 
 
-def get_data(data):
-    for i in range(1,len(data)):
-        for k in range(len(data)-i):
-            if data[k]>data[k+1]:
-                a=data[k]
-                data[k]=data[k+1]
-                data[k + 1]=a
-li = get_random_list(10)
-print(li)
-get_data(li)
-print(li)
-# for i in range(1,len(li)):
-#     for j in range(len(li) - i):
-#         if li[j] > li[j + 1]:
-#             temp = li[j]
-#             li[j] = li[j + 1]
-#             li[j + 1] = temp
-# print(li)
-
-#
-# a='1'
-# b='2'
-# c=b and a
-# print('%(b)s,%(a)s'%dict(a=a,
-#                          b=b))
-
-
-# @paramunittest.parametrized(*case)
+# Case_xls = common.get_xls("gzfxCase.xlsx", "getgzsjlist")
+# @paramunittest.parametrized(*Case_xls)
 # class testlist(unittest.TestCase):
 #     def setParameters(self, a, b, c, d, e, f, g):
 #         self.a=a
@@ -64,33 +24,54 @@ print(li)
 #         self.f=f
 #         self.g=g
 #
-#     def test_list(self):
-#         print(self.a,self.b,self.d,self.d,self.e,self.f,self.g)
-#     def tearDown(self):
-#         """
+#     @classmethod
+#     def setUpClass(cls) -> None:
+#         print('setUpClass')
 #
-#         :return:
-#         """
-#         pass
+#     def setUp(self) -> None:
+#         print('setUp')
+#     def test_1(self):
+#         print('test_1')
+#         print(self.a,self.b,self.d,self.d,self.e,self.f,self.g)
+#     def test_2(self):
+#         print('test_2')
+#         print(self.a,self.b,self.d,self.d,self.e,self.f,self.g)
+#     def tearDown(self) -> None:
+#         print('tearDown')
+#
+#     @classmethod
+#     def tearDownClass(cls) -> None:
+#         print('tearDownClass')
 
+Case_xls = Get_excel.read_casefile_2("gzfxCase.xlsx", "getgzsjlist")
+@ddt.ddt
+class testlist(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        print('setUpClass')
 
-# @ddt.ddt
-# class testlist(unittest.TestCase):
-#     @ddt.data(*Case_xls)
-#     # @ddt.unpack
-#     def setdate(self,data):
-#         self.case_name = data['case_name']
-#         self.berthcode = data['case_name']
-#         self.vehicletype = data['vehicletype']
-#         self.carproperty = data['carpropertycarproperty']
-#         self.dotime = data['dotimedotime']
-#         self.endtime = data['endtime']
-#         self.msg = data['message']
-#         self.response = None
-#         self.info = None
-#     def setUp(self):
-#         self.setdate()
-#     def test_list(self):
-#         print(self.case_name)
-# if __name__ == '__main__':
-#     unittest.main()
+    def setUp(self) -> None:
+        print('setUp')
+    @ddt.data(*Case_xls)
+    # @ddt.unpack
+    def test_1(self,data):
+        print('test_1')
+        print(data)
+        self.case_name = data['case_name']
+        self.berthcode = data['method']
+        self.vehicletype = data['cookie']
+        self.carproperty = data['data']
+        self.dotime = data['result']
+        self.endtime = data['code']
+        self.msg = data['msg']
+
+    def test_2(self):
+        print('test_2')
+    def tearDown(self) -> None:
+        print('tearDown')
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print('tearDownClass')
+if __name__ == '__main__':
+    unittest.main()
